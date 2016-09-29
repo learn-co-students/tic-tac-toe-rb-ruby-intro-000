49f440d99e202b313fc78e1024b598075faee32c
Christiane Heiligers <tina.heiligers@gmail.com>
2016-09-29 17:06:44 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,113 @@
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
+# Helper Methods
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+# play a turn methods START
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+# play a turn methods END
+
+# play a turn START
+def turn(board)
+  puts "Please enter 1-9:"
+  position = gets.strip
+  index = input_to_index(position)
+  if valid_move?(board, index)
+    current_player = current_player(board)
+    move(board, index, current_player)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+# play a turn END
+
+#decide curent player methods START
+def turn_count(board)
+  count = 0
+  board.each do |entry|
+    if (entry == "X" || entry == "O")
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
+#decide curent player methods END
+
+# Define your play method below
+def play(board)
+  # will change to until over?(board)
+  # check who's turn it is
+  # set the token appropriately
+  # play a turn
+  # if over?(board),
+  # see what ended the game,
+  # if won, announce winner,
+  # otherwise announce a draw.
+  counter = 0
+  until counter == 9
+    counter += 1
+    turn(board)
+  end
+end
+
+# game status methods
+def won?(board)
+  WIN_COMBINATIONS.each do |indices|
+    result = indices.map { |i| board[i] }
+    if result == ["X", "X", "X"] || result == ["O", "O", "O"]
+      return indices
+    end
+  end
+  false
+end
+
+def full?(board)
+  !board.any? { |index| index == " " || index == "" || index == nil}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  draw?(board) || won?(board) || full?(board)
+end
+
+def winner(board)
+  won?(board) ? board[won?(board)[0]] : nil
+end
