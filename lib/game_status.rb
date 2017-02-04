8e8574a117a151d376ba429205bc8a694996e5d5
Brian Wilson <wilson.brian90@gmail.com>
2017-02-04 23:45:19 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,58 +1,3 @@
-# Helper Methods
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, token)
-  board[index] = token
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  token = "O"
-  if turn_count(board).even?
-    token = "X"
-  end
-  if valid_move?(board, index)
-    move(board, index, token)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-# Define your play method below
-def play(board)
-until over?(board) do
-turn(board)
-end
-
-if won?(board)
-  winner = winner(board)
-  puts "Congratulations #{winner}!"
-elsif draw?(board)
-  puts "Cats Game!"
-end
-end
-
 
 def turn_count(board)
   turns = 0
@@ -132,19 +77,13 @@ end
 def draw?(board)
 if full?(board) && !(won?(board))
   return true
-elsif won?(board)
-  return false
 else
   return false
 end
 end
 
 def over?(board)
-  if draw?(board)
-    return true
-  elsif won?(board)
-    return true
-  elsif full?(board)
+  if draw?(board) || won?(board)
     return true
   else
     return false
