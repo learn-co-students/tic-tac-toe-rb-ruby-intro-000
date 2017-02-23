ea5071143d81e8842fc9ea44992b5a86e6f28c51
Anna Olshevskaia <olshevskaia.anna@gmail.com>
2017-02-23 21:01:46 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,105 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def valid_move?(board, index)
+  index != -1 && (board[index] == "" || board[index] == " " || board[index] == nil)
+end
+
+def move(board, index, value)
+  board[index] = value
+end
+
+def turn(board)
+  index = -1
+  loop do
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      break
+    end
+  end
+  move(board, index, current_player(board))
+  display_board(board)
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |value|
+    if value == "X" || value == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combination|
+    value = board[combination[0]]
+    if value == "X" || value == "O"
+      combination.all? {|index| board[index] == value}
+    end
+  end
+end
+
+def full?(board)
+  board.all? {|value| !(value.nil? || value == " ")}
+end
+
+def draw?(board)
+  ! won?(board) && full?(board)
+end
+
+def over?(board)
+  draw?(board) || won?(board)
+end
+
+def winner(board)
+  won = won?(board)
+  if won
+    board[won[0]]
+  end
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  else
+    puts "Cats Game!"
+  end
+end
