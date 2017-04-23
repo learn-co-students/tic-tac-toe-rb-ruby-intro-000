c1ab5e69e845e4b777be74a4de4769ccb93408d7
Robert Downey <countrysc@yahoo.com>
2017-04-23 18:55:47 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,11 +1,3 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -17,12 +9,41 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def input_to_index(user_input)
-  index = user_input.to_i - 1
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board,index)
-  index.between?(0,8) && !position_taken(board,index)  
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move (board,index,player)
+  board[index] = player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, position)
+  position.between?(0,8) && !position_taken?(board, position)  
+end
+
+def turn(board)
+  puts "Where would you like to go?"
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board,index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
 end
 
 def won?(board)
@@ -32,71 +53,42 @@ def won?(board)
 end
 
 def full?(board)
-  board.none? do |empty|
-    empty == " " || empty.nil?
-  end
+  !board.include? " "
 end
 
 def draw?(board)
-  won?(board) == nil && full?(board) == true
+  !won?(board) && full?(board)
 end
 
 def over?(board)
-  won?(board) == true || full?(board) == true
-end
-
-def turn(board)
-  puts "Where would you like to go?"
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  validity = valid_move?(board,index)
-  while validity != true
-    puts "Invalid move.  Please enter 1-9:"
-    user_input = gets.strip
-    index = input_to_index(user_input)
-    validity = valid_move?(board,index)
-  end
-  move(board,index,current_player(board))
-end
-
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+  won?(board) || draw?(board)
 end
 
 def current_player(board)
   count = turn_count(board)
-  if count % 2 == 0
-    "X"
+  if count.even?
+    return "X"
   else
-    "O"
+    return "O"
   end
 end
 
 def turn_count(board)
-  counter = 0
-  board.each do |turn|
-    if turn == "X" || turn == "O"
-      counter +=1
-    end
-  end
-  counter
-end
-
-def move (board,index,character)
-  board[index] = character
-  turn_count(board)
-  display_board(board)
+  board.count { |x| x == "X" || x == "O" }
 end
 
 def winner(board)
-  if won?(board) != nil
-    winner = board[won?(board)[0]]
+  if won?(board) == nil
+    nil
+  elsif board[won?(board)[0]] == "X"
+    "X"
+  elsif board[won?(board)[0]] == "O"
+    "O"
   end
 end
 
 def play(board)
-  until over?(board) == true
+  while !over?(board)
     turn(board)
   end
   if draw?(board)
