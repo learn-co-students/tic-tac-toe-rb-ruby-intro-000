954818f48429952ad1bd120eb06cf8ce171eb9af
Jason Tseng <jason.f.tseng@gmail.com>
2017-04-23 13:43:35 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,140 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board(board)
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row2 = "-----------"
+  row3 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row4 = "-----------"
+  row5 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts row1
+  puts row2
+  puts row3
+  puts row4
+  puts row5
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(array, index, character)
+  array[index] = character
+end
+
+def position_taken?(board, index)
+  board[index] != " " && board[index] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  character = current_player(board)
+  if valid_move?(board, index) == true
+    move(board, index, character)
+  else
+    until valid_move?(board,index) == true
+      puts "Please input 1-9:"
+      user_input = gets.strip
+      index = input_to_index(user_input)
+      valid_move?(board, index)
+    end
+    move(board,index, character)
+  end
+  display_board(board)
+end
+
+def turn_count(board)
+counter = 0
+  board.each do |index|
+    if index != " "
+        counter += 1
+    end
+  end
+    counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 1
+    return "O"
+  else
+    return "X"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      win_combination
+    end
+  end
+end
+
+def full?(board)
+  board.all? do |index|
+    index == "X" || index =="O"
+  end
+end
+
+def draw?(board)
+  if won?(board) == nil && full?(board) == true
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) != nil
+    true
+  elsif draw?(board) == true
+    true
+  else full?(board) == false
+    false
+  end
+end
+
+def winner(board)
+  if over?(board) == true && won?(board) != nil
+    win_char = won?(board)
+    board[win_char[0]]
+  else
+    nil
+  end
+end
+
+def play(board)
+  until over?(board) == true do
+    turn(board)
+    over?(board)
+  end
+
+  if won?(board) != nil
+    winner = winner(board)
+    puts "Congratulations #{winner}!"
+  elsif draw?(board) == true
+    puts "Cats Game!"
+  end
+end
