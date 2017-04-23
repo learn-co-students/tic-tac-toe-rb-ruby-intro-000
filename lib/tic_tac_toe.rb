1345cf3b7f90c8119172bb40f4b392d89b80b54a
Robert Downey <countrysc@yahoo.com>
2017-04-23 16:29:10 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,6 +9,14 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def input_to_index(input)
   index = input.to_i - 1
 end
