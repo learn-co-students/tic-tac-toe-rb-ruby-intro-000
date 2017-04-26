fdd6951f2480cd94125ee3d474ac81ced7030e3c
Samson Yuwono <samson.yuwono@gmail.com>
2017-04-26 19:17:12 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,7 +52,7 @@ def turn(board)
   user_input= gets.strip
   index = input_to_index(user_input)
     if valid_move?(board, index)== true
-       move(board, index, character="X")
+       move(board, index, current_player(board))
        display_board(board)
     else
       turn(board)
