b2d35cdafccb940a15967a9396e95082a3f0594a
Rob Ellis <rob.ellis357@gmail.com>
2017-05-20 18:14:34 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,4 @@
-# Define your WIN_COMBINATIONS constant
-#
+#Winning COmbos
 WIN_COMBINATIONS = [
 	[0,1,2],
 	[6,7,8],
@@ -11,7 +10,45 @@ WIN_COMBINATIONS = [
 	[2,4,6]
 ]
 
-winner_val =
+# Define your play method below
+def play(board)
+	while !over?(board)
+    turn(board)
+	end
+	if won?(board)
+		puts "Congratulations #{winner(board)}!"
+	elsif draw?(board)
+		puts "Cats Game!"
+	end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
+  end
+end
+
+def full?(board)
+	   if board.select {|item| item == " "} != []
+	       return false
+	   else
+	       return true
+	   end
+end
+
+def draw?(board)
+		!won?(board) && full?(board)
+end
 
 def won?(board)
     WIN_COMBINATIONS.each do | wins |
@@ -26,30 +63,9 @@ def won?(board)
        return false
 end
 
-def full?(board)
-    if board.select {|item| item == " "} != []
-        return false
-    else
-        return true
-    end
-end
-
-def draw?(board)
-    if full?(board) == true && won?(board) == false
-        return true
-    else
-        return false
-    end
-end
-
 def over?(board)
-    if won?(board) != false || draw?(board) == true
-        return true
-    else
-        return false
-    end
+	won?(board) || draw?(board)
 end
-
 
 def winner(board)
   if won?(board) == false
@@ -61,40 +77,28 @@ def winner(board)
   end
 end
 
+#board = ["O","O","O","O","O","O","O","O","O"]
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-board = ["O","O","O","O","O","O","O","O","O"]
-
-display_board(board)
-
-# code your input_to_index and move method here!
-
+#display_board(board)
 
 def input_to_index(user_input)
   user_input = user_input.to_i - 1
 end
 
-def move(board, user_input, char = "X")
+def move(board, user_input, char) #char = "X"
   board[user_input] = char
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  input = input_to_index(input)
-  if valid_move?(board, input)
-    move(board, input)
-  else
-    turn(board)
-  end
-  display_board(board)
+	puts "Please enter 1-9:"
+	user_input = gets.strip
+	index = input_to_index(user_input)
+	if valid_move?(board, index)
+		move(board, index, current_player(board))
+		display_board(board)
+	else
+		turn(board)
+	end
 end
 
 def position_taken?(board, index)
@@ -105,20 +109,6 @@ def position_taken?(board, index)
   end
 end
 
-position_taken?(board,0)
-
-# code your #valid_move? method here
-def valid_move?(board, index)
-  if index.between?(0,8)
-    if !position_taken?(board, index)
-      true
-    end
-  end
-  #index.between?(0,8) && !position_taken?(board, index)
-end
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   board[index] != " "
 end
@@ -133,23 +123,10 @@ def turn_count(board)
   return count
 end
 
-turn_count(board)
-
 def current_player(board)
   if turn_count(board) % 2 == 0
     return "X"
   else
     return "O"
-  end
-end
-
-current_player(board)
-
-# Define your play method below
-def play(board)
-  count = 0
-  while count != 9
-    turn(board)
-    count += 1
-  end
+	end
 end
