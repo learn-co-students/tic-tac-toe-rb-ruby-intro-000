0b5cd5fa6e76c9b48ec411201ab17466ef1b47b8
Barney Fabian <bafa@tutanota.de>
2018-01-06 20:52:10 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,17 +28,13 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  elsif board [index] == "X" || board[index] == "O"
+  else
     true
   end
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == false && index.between?(0,8)
-    true
-  else
-    false
-  end
+  position_taken?(board, index) == false && index.between?(0,8)
 end
 
 def turn(board)
