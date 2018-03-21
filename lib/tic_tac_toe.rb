221fa2d3545bb1eb3249442514bcef7c18dbe1e3
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-21 19:44:01 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,102 @@
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
 
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_comb|
+    if board[win_comb[0]] + board[win_comb[1]] + board[win_comb[2]] == "XXX" || board[win_comb[0]] + board[win_comb[1]] + board[win_comb[2]] == "OOO"
+      return win_comb
+    end
+  end
+  return false
+end
+
+def full?(board)
+  (0..8).all? {|index| position_taken?(board, index)}
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  end
+  return false
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    return true
+  end
+  return false
+end
+
+def winner(board)
+  if !won?(board)
+    return nil
+  else
+    return board[won?(board)[0]]
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |place|
+    if place == "X" || place == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+# def position_taken?(board, location)
+#  board[location] != " " && board[location] != ""
+# end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+  until over?(board) do
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
