7bb2dadf100dcff12a0e573dfe95f665dc004115
AL <nela.asus+dvcs@gmail.com>
2018-10-03 15:42:57 -0400

    define WIN_COMBINATIONS test passed

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,13 @@
+
+
+# Define win combinations
+WIN_COMBINATIONS = [
+  [0,1,2], # Top horizontal
+  [3,4,5], # Middle horizontal
+  [6,7,8], # Bottom horizontal
+  [0,3,6], # Left vertical
+  [1,4,7], # Middle vertical
+  [2,5,8], # Right vertical
+  [0,4,8], # \ diagonal
+  [2,4,6] # / diagonal
+]
