57877d8cbfa7d31496522bfc23691b14a3bc37eb
AL <nela.asus+dvcs@gmail.com>
2018-10-04 14:53:34 -0400

    redefined #display_board

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,11 +16,14 @@ WIN_COMBINATIONS = [
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] 
 
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-"*11
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-"*11
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  line = lambda { |x,y,z| " %s | %s | %s " % [x,y,z] }
+  dashes = "-"*11
+  
+  puts line[ board[0], board[1], board[2] ]
+  puts dashes
+  puts line[ board[3], board[4], board[5] ]
+  puts dashes
+  puts line[ board[6], board[7], board[8] ] 
 end
 
 # Convert user input to valid board index
