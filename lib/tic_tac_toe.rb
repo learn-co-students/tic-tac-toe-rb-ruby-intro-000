a8e5dae08af09b3c2c0dd23735515803c9937eab
shivani brijmohan <sbrij001@yahoo.com>
2018-11-26 23:49:04 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,8 +16,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
+def input_to_index(input)
+  input = (input.to_i) - 1
 end
 
 def move(board, index, current_player = "X")
