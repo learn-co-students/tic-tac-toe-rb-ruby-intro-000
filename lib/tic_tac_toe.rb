367032e8b3b5fefaf73f729f12def8011d4c1b17
Aaron Blum <blumaa@gmail.com>
2019-07-10 20:17:52 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,31 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, user_input)
+  board[index] = user_input
+end
+
+  puts user_input.to_i
+end
