dd89ecce4b924541daedb0524fabbefc7698603b
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-01-29 19:00:35 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,7 +22,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
+def move(board, index, current_player)
   board[index] = current_player
 end
 
