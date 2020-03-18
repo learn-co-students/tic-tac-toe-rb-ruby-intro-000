290727a5f39c4e45fc1d0e7e30987ff1ba5342b1
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-03-18 00:17:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,7 +97,7 @@ def play(board)
   until over?(board)
     turn(board)
   if won?(board)
-    puts "Congrats #{winner(board)}"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's Game!"
   end
