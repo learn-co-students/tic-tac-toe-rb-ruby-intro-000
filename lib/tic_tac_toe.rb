8b968821d32db0d9cf623283f8476a825c9d28df
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-03-18 00:09:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,7 +45,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
