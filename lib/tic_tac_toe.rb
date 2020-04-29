6b6ec89dd55211b97cb2f0f31f5d783496e22770
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-29 21:20:23 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,116 @@
+WIN_COMBINATIONS = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[6,4,2]
+]
+
+def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+   user_input.to_i - 1
+end
+
+def move(board, index, player)
+   board[index] = player
+end
+
+def position_taken?(board, index, player)
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false
+   else
+      return true
+   end
+end
+
+def valid_move?(board, index, player)
+   if index.between?(0,8) && !position_taken?(board, index, player)
+      return true
+   end
+end
+
+def turn(board)
+   puts "Please enter 1-9:"
+   user_input = gets.strip
+   index = input_to_index(user_input)
+   if valid_move?(board, index)
+      move(board, index, current_player(board))
+      turn(board)
+   end
+   display_board(board)
+end
+
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+end
+def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+ WIN_COMBINATIONS.detect do |win_combination|
+  board[win_combination[0]] == board[win_combination[1]] &&
+  board[win_combination[1]] == board[win_combination[2]] &&
+  position_taken?(board, win_combination[0])
+ end
+
+def full?(board)
+   board.all? {|i| i == "X" || i == "O"}
+end
+
+def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif !won?(board) && !full?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
+
+def over?(board)
+   if draw?(board) || won?(board) || full?(board)
+     return true
+   end
+end
+
+def winner(board)
+   if won?(board)
+   return board[won?(board)[0]]
+   end
+end
+
+def play(board)
+   counter = 0
+   until counter == 9
+     turn(board)
+     counter += 1
+   end
+end
+
+def play(board)
+   until over?(board)
+      turn(board)
+   end
+   if won?(board)
+      winner(board) == "X" || winner(board) == "O"
+      puts "Congratulations #{winner(board)}!"
+   elsif draw?(board)
+      puts "Draw"
+   end
+end
