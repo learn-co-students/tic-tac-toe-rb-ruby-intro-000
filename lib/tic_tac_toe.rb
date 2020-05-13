cce1b643c2968978a6721f55033fd56497b49c9e
Anthony Miguez <anm501@stern.nyu.edu>
2020-05-13 00:18:08 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,7 +29,6 @@ end
 
 def move(board, index, character)
   board[index] = character
-  return board
 end
 
 
@@ -47,8 +46,6 @@ end
 def valid_move?(board,index)
   if index.between?(0,8) && !position_taken?(board,index)
     return true
-  else
-    return false
   end
 end
 
@@ -58,8 +55,8 @@ def turn(board)
   num = gets.to_i
   index = input_to_index(num)
   if valid_move?(board,index)
-    move(board,index,character)
-    puts display_board(board)
+    move(board, index, current_player(board))
+    display_board(board)
   else
     until valid_move?(board,index)
       puts "Please enter 1-9:"
@@ -82,8 +79,8 @@ end
 
 
 def current_player(board)
-  num = turn_count(board)
-  if num % 2 == 0
+  num_ = turn_count(board)
+  if num_% 2 == 0
     return "X"
   else
     return "O"
@@ -117,23 +114,26 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     true
+  end
 end
 
 
 def over?(board)
   if won?(board) || full?(board) || draw?(board)
     true
+  end
 end
 
 
 def winner(board)
   if win_combo = won?(board)
     board[win_combo.first]
+  end
 end
 
 
 def play(board)
-  until over?(board) do
+  until over?(board)
     turn(board)
   end
   if won?(board)
