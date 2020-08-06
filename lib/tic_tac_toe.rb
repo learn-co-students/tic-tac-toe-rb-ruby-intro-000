3a2dab3e6a17a6dde33fcf5199191e6f73b7a376
DavidRMorphew <morphewd@umich.edu>
2020-08-06 13:54:40 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,3 +24,6 @@ WIN_COMBINATIONS = [
   def position_taken?(board, position)
     !(board[position] != "X" && board[position] != "O")
   end
+  def valid_move?(board, position)
+    position.between(0,8) && !position_taken?(board)
+  end
