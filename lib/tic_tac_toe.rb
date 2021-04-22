0233c5aec35685d61650ac3d4feb6475be09fc01
Jeffrey Martinez <martinezjf2@gmail.com>
2021-04-21 23:11:35 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,6 +45,7 @@ def turn(board)
     index = input_to_index(user_input)
     if valid_move?(board, index)
         move(board, index, current_player(board))
+        display_board(board)
     else
         turn(board)
     end
